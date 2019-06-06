813da972cce6efed62d84388104f37d42846ad28
glancer000 <glancer000@gmail.com>
2019-06-05 23:05:20 -0300

    netlify form code

--- src/pages/index.js
+++ src/pages/index.js
@@ -98,20 +98,36 @@ class Homepage extends React.Component {
                         </header>
                         <p>Contact us to recieve a free quote and timeline for your project</p>
 
-                        <form name="contact" method="POST" data-netlify="true">
-                                <ul className="actions uniform">
-                            <li><input 
-                                type="text"
-                                name="name"
-                                placeholder="your name"/></li>
-                            <li><input 
-                                type="email"
-                                name="email"
-                                placeholder="your email"/></li>
-                        </ul>                 
-                                
-                            <button type="submit">Send</button>
+                        <div >
+                        <form 
+                            name="contact"
+                            method="post"
+                            data-netlify="true"
+                            data-netlify-honeypot="bot-field"
+                            
+                            >
+                            <input type="hidden" name="bot-field" />
+                            <input type="hidden" name="form-name" value="contact" />
+                        <div className="field half first">
+                            <label htmlFor="name">Name</label>
+                            <input type="text" name="name" id="name" />
+                        </div>
+                        <div className="field half">
+                            <label htmlFor="email">Email</label>
+                            <input type="text" name="email" id="email" />
+                        </div>
+                        <div className="actions">
+                            <label htmlFor="message">Message</label>
+                            <textarea name="message" id="message" rows="6" />
+                        </div>
+                        <ul className="actions">
+                            <li>
+                            <input type="submit" value="Send Message" className="special" />
+                            </li>
+                            <li> <input type="reset" value="Clear" /></li>
+                        </ul>
                         </form>
+                        </div>
                     </div>
                 </section>
             </Layout>
