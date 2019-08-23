b3e532d778254aa7e35ec7c8734e0c740f3099b4
samuel-chris <prosam001@gmail.com>
2019-08-23 16:36:56 +0100

    Style changes

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './footer-styles.scss'
 import InstagramLogo from '../../assets/images/instagram.svg'
-import MediumLogo from '../../assets/images/medium.svg'
+import MediumLogo from '../../assets/images/medium.png'
 import LinkedinLogo from '../../assets/images/linkedin.svg'
 
 class Footer extends React.Component {
@@ -26,9 +26,9 @@ class Footer extends React.Component {
                     <div className="col-4 social">
                         <p>Follow Us</p>
                         <ul className="follow">
-                            <li><a href="#" className="icon"><object data={InstagramLogo} type="image/svg+xml"></object></a></li>
-                            <li><a href="#" className="icon"><object data={LinkedinLogo} type="image/svg+xml"></object></a></li>
-                            <li><a href="#" className="icon"><object data={MediumLogo} type="image/svg+xml"></object></a></li>
+                            <li><a href="#" className="icon"><object data={InstagramLogo} type="image/svg+xml">Instagram</object></a></li>
+                            <li><a href="#" className="icon"><object data={LinkedinLogo} type="image/svg+xml">Linkedin</object></a></li>
+                            <li><a href="#" className="icon"><img src={MediumLogo} alt="Medium"/></a></li>
                         </ul>
                     </div>
                 </div>
