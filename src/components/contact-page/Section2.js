8a30cc2aedfa1cd1ce41250c711b7c2b412c950f
samuel-chris <prosam001@gmail.com>
2019-08-30 23:06:13 +0100

    changes

--- src/components/contact-page/Section2.js
+++ src/components/contact-page/Section2.js
@@ -7,7 +7,7 @@ export default function Section2() {
         <div className="hero contact-section2">
             <div className="grid-wrapper">
                 <div className="text col-6 col-sm-12">
-                    <p>Let’s Chat!</p>
+                    <p class="lead">Let’s Chat!</p>
                     <h1 className="lead">Contact Us</h1>
                     <p>Download our cost calculator app! Not only will you be able to test out performance a Flutter app is, but you will be able to compare our development costs compared to native development.</p>
                 </div>
