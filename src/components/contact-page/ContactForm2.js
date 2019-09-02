d11d57ed38429a99f463664150d782ce3fb7626e
samuel-chris <prosam001@gmail.com>
2019-09-02 16:08:56 +0100

    form changes

--- src/components/contact-page/ContactForm2.js
+++ src/components/contact-page/ContactForm2.js
@@ -6,9 +6,9 @@ export default function ContactForm2() {
     <form method="POST" name="contact-form" data-netlify="true" data-netlify-honeypot="bot-field">
       <input type="hidden" name="form-name" value="contact-form" />
       <label htmlFor="name">Name</label>
-        <input type="text" name="name" placeholder="John Doe" className="input" maxlength="50" required/>
+        <input type="text" name="name" placeholder="John Doe" className="input" maxlength="75" required/>
       <label htmlFor="email">Email</label>
-        <input type="email" name="email" placeholder="Example@email.com" className="input" maxlength="50" required/>
+        <input type="email" name="email" placeholder="Example@email.com" className="input" maxlength="75" required/>
       <label htmlFor="comments">Comments</label>
         <textarea name="comments" rows="5" placeholder="Let us know how we can help" class="input" maxlength="500" required></textarea>
       <label htmlFor="options">Where did you hear about us?</label>
