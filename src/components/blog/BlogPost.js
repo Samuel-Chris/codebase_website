02f408c9c87cd50ed3eb4c9e8ca74cb56ba3f82d
samuel-chris <prosam001@gmail.com>
2019-09-14 18:43:17 +0100

    initial commit

--- src/components/blog/BlogPost.js
+++ src/components/blog/BlogPost.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './blog-styles.scss'
 import {Link} from 'gatsby'
 import Img from "gatsby-image"
+import TextTruncate from 'react-text-truncate';
 
 export default function BlogPost(props) {
     const {content, img} = props;
@@ -11,8 +12,10 @@ export default function BlogPost(props) {
             <Img fluid={img} alt=""/>
             <div className="card-content">
                 <p className="lead dev">{post.frontmatter.topic}</p>
-                <h3 className="lead title">{post.frontmatter.title}</h3>
-                <p className="text">{post.excerpt}</p>
+                {/* <h3 className="lead title">{post.frontmatter.title}</h3> */}
+                <TextTruncate line={2} element="h3" truncateText="..." text={post.frontmatter.title} containerClassName="lead"/>
+                {/* <p className="text">{post.excerpt}</p> */}
+                <TextTruncate line={4} element="p" truncateText="..." text={post.excerpt} containerClassName="text"/>
                 <p className="lead read-more">
                     <Link to={post.fields.slug}>Read more</Link>
                 </p>
