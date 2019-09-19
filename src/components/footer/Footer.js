113f6ef68aaf89c60bda336d3945b51d5fb5dd64
samuel-chris <prosam001@gmail.com>
2019-09-19 23:10:57 +0100

    Links

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -26,8 +26,8 @@ class Footer extends React.Component {
                     <div className="col-4 social">
                         <p>Follow Us</p>
                         <ul className="follow">
-                            <li><a href="https://www.youtube.com/channel/UCxo3YOwdU_ygBBv74gDjmxA" className="icon"><img src={YTLogo} alt="Youtube"/></a></li>
-                            <li><a href="https://linkedin.com/company/codebase-studio/about/?viewAsMember=true" className="icon"><object data={LinkedinLogo} type="image/svg+xml">Linkedin</object></a></li>
+                            <li><a href="https://www.youtube.com/channel/UCxo3YOwdU_ygBBv74gDjmxA" target="_blank" className="icon"><img src={YTLogo} alt="Youtube"/></a></li>
+                            <li><a href="https://linkedin.com/company/codebase-studio/about" target="_blank" className="icon"><object data={LinkedinLogo} type="image/svg+xml">Linkedin</object></a></li>
                             <li><a href="https://medium.com/@codebase.studio" target="_blank" className="icon"><img src={MediumLogo} alt="Medium"/></a></li>
                         </ul>
                     </div>
