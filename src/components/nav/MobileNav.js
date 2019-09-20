3f24146a701c048703a2763307538a6f3b9e5514
samuel-chris <prosam001@gmail.com>
2019-09-20 09:36:25 +0100

    links

--- src/components/nav/MobileNav.js
+++ src/components/nav/MobileNav.js
@@ -40,7 +40,7 @@ export default class MobileNav extends Component {
 
                     <div className={"hidden " + visible}>
                         <ul>
-                            <li><Link to="/">Why Choose Us</Link></li>
+                            <li><h3><Link to="/">Why Choose Us</Link></h3></li>
                             <li><h3><Link to="/pricing">Pricing</Link></h3></li>
                             <li><h3><Link to="/process">Process</Link></h3></li>
                         </ul>
